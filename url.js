26e42f1e5360d1945004bf505045a3f91cd0b732
hstakaamy <133208300+hstakaamy@users.noreply.github.com>
2026-05-29 22:26:52 +0800

    Fix formatting in url.js

--- url.js
+++ url.js
@@ -1,6 +1,8 @@
 const urls = [
-    'https://www.surveycake.com/s/AvOwa',
+    'https://www.surveycake.com/s/AvOwa', 
     'https://www.surveycake.com/s/mYpdB',
     'https://www.surveycake.com/s/2kL2r',
     'https://www.surveycake.com/s/k8p1Z'
 ];
+
+#一般x經驗、一般x搜尋、爆炸x經驗、爆炸x搜尋
